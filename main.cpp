91bbab67953fea940bf2f5cdbe8ae5c4ef2eb103
Dixaba <dixaba@yandex.ru>
2019-03-31 03:09:19 +0300

    Переводик Qtовых менюшек

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+  QApplication a(argc, argv);
+  QTranslator qtTranslator;
+  qtTranslator.load("qt_" + QLocale::system().name(),
+                    QString(":/translations"));
+  a.installTranslator(&qtTranslator);
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
